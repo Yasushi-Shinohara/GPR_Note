15861ab1e5ae0c03151bb0c99733dc59953808c6
Yasushi SHINOHARA <yasushi.shinohara@gmail.com>
2020-08-20 04:27:07 +0900

    Polynomial fitting is added.

--- common/functions.py
+++ common/functions.py
@@ -57,3 +57,22 @@ class Gaussian_Process_Regression():
             mean = mean*(ymax - ymin) + ymid
             std = std*(ymax - ymin)
         return mean, std
+#
+def PF(_xsample, _ysample, _x, _deg, normalization = True):
+    if (normalization):
+        xmin = np.amin(_x)
+        xmax = np.amax(_x)
+        xmid = 0.5*(xmin + xmax)
+        _x = (_x - xmid)/(xmax - xmin)
+        _xsample = (_xsample - xmid)/(xmax - xmin)
+        ymin = np.amin(_ysample)
+        ymax = np.amax(_ysample)
+        ymid = 0.5*(ymin + ymax)
+        _ysample = (_ysample - ymid)/(ymax - ymin)
+    Nsample = len(_xsample)
+    polyinfo = np.polyfit(_xsample, _ysample, _deg)
+    p = np.poly1d(polyinfo)
+    mean = p(_x)
+    if (normalization):
+        mean = mean*(ymax - ymin) + ymid
+    return mean
