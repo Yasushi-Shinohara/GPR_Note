135f035238ab07fe740869da3983f4c69106c4b6
Yasushi SHINOHARA <yasushi.shinohara@gmail.com>
2020-08-21 05:07:35 +0900

    Bayes optimization with fixed kernel is installed.

--- common/functions.py
+++ common/functions.py
@@ -76,3 +76,33 @@ def PF(_xsample, _ysample, _x, _deg, normalization = True):
     if (normalization):
         mean = mean*(ymax - ymin) + ymid
     return mean
+#
+class  Bayesian_opt():
+    def __init__(self):
+        self.aqui_name = 'PI' #'PI', 'EI', 'UCB'
+        self.xi = 0.01
+
+#### PI
+    def aqui_PI(self, mean, std, maxval):
+        Z = (mean -  maxval - self.xi)/std
+        return norm.cdf(Z)
+#### EI
+    def aqui_EI(self, mean, std, maxval):
+        Z = (mean -  maxval - self.xi)/std
+        return (mean - maxval - self.xi)*norm.cdf(Z) + std*norm.pdf(Z)
+#### UCB
+    def aqui_UCB(self, mean, std, maxval):
+        return mean + 1.0*std
+
+    def get_aqui(self, mean, std, maxval):
+        if (self.aqui_name == 'PI'):
+            aqui = self.aqui_PI(mean, std, maxval)
+        elif (self.aqui_name == 'EI'):
+            aqui = self.aqui_EI(mean, std, maxval)
+        elif (self.aqui_name == 'UCB'):
+            aqui = self.aqui_UCB(mean, std, maxval)
+        else:
+            print('# ERROR: undefined acquisition function called.')
+            sys.exit()
+
+        return aqui
