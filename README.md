8a425accc171d155fe9566f7d4ce248a28593d6e
Yasushi SHINOHARA <yasushi.shinohara@gmail.com>
2020-11-02 19:59:46 +0900

    The code is updated to be with BayesianOptimization code. Several typos are fixed and organizations are performed.

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # GPR_Note
 
-Very simple Gaussian process regression (GPR) algorithm is written in the codes, `Poject1`. The code is applied to Bayesian optimization (BO) as well with very simple acquistion functions, `Project2`.
-GPR with scikit-learn implementation is also given in `Project3` directory and the combination to BO in `Prject4`.
+Very simple Gaussian process regression (GPR) algorithm is written in `common/homemade_GPR.py`. The usage of the homemad GPR is in the codes, `Poject1`. Also, homemade Bayesian optimization (BO) algorithm is coded in `common/homemad_BO.py`. Comination of two homemade implementation is in `Projcet2`.
+
+GPR of scikit-learn and its combination to BO are placed in `Project3` and `Project4`. The interface for our purpose is in `common/SKL_GPR.py`.
+
+BO is also provided by another code, BayesiaonOptimizaiton. DEMO with the same example is written in `Project5`.
 
 # Requirements 
  
 * numpy
 * scipy
 * matplotlib
-* scikit-learn: for sklearn.gaussian_process
+* scikit-learn: for sklearn.gaussian_process for `Project3` and `Project4`
+* BayesianOptimization for `Project5`
 
 # Author
 
--- common/homemade_BO.py
+++ common/homemade_BO.py
@@ -2,30 +2,30 @@ from scipy.stats import norm
 #
 class  Bayesian_opt():
     def __init__(self):
-        self.aqui_name = 'PI' #'PI', 'EI', 'UCB'
+        self.acqui_name = 'PI' #'PI', 'EI', 'UCB'
         self.xi = 0.01
 
 #### PI
-    def aqui_PI(self, mean, std, maxval):
+    def acqui_PI(self, mean, std, maxval):
         Z = (mean -  maxval - self.xi)/std
         return norm.cdf(Z)
 #### EI
-    def aqui_EI(self, mean, std, maxval):
+    def acqui_EI(self, mean, std, maxval):
         Z = (mean -  maxval - self.xi)/std
         return (mean - maxval - self.xi)*norm.cdf(Z) + std*norm.pdf(Z)
 #### UCB
-    def aqui_UCB(self, mean, std, maxval):
+    def acqui_UCB(self, mean, std, maxval):
         return mean + 1.0*std
 
-    def get_aqui(self, mean, std, maxval):
-        if (self.aqui_name == 'PI'):
-            aqui = self.aqui_PI(mean, std, maxval)
-        elif (self.aqui_name == 'EI'):
-            aqui = self.aqui_EI(mean, std, maxval)
-        elif (self.aqui_name == 'UCB'):
-            aqui = self.aqui_UCB(mean, std, maxval)
+    def get_acqui(self, mean, std, maxval):
+        if (self.acqui_name == 'PI'):
+            acqui = self.acqui_PI(mean, std, maxval)
+        elif (self.acqui_name == 'EI'):
+            acqui = self.acqui_EI(mean, std, maxval)
+        elif (self.acqui_name == 'UCB'):
+            acqui = self.acqui_UCB(mean, std, maxval)
         else:
             print('# ERROR: undefined acquisition function called.')
             sys.exit()
 
-        return aqui
+        return acqui
